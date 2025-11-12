4b491442b03cfbd0878601c63f03e3494f84fd7a
Alireza <alireza.oftadeh.7842@gmail.com>
2025-11-12 10:02:27 +0100

    Add initial HTML structure and CSS styles for the Radio Gaga website

---
+++ radio.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Radio Gaga - Radio</title>
+        <link rel="stylesheet" href="CSS\styles.css">
+    </head>
+    <body>
+        <div id = "navbar">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="radio.php">Radio</a></li>
+                <li><a href="playlist.php">Playlist</a></li>
+                <li><a href="contact.php">Contact</a></li>
+            </ul>
+        </div>
+        <div id="welcome-section">
+            <h1>Welcome to Radio Gaga</h1>
+            <p>This is a sample webpage created using PHP and HTML.</p>
+        </div>
+    </body>
+</html>
